05b405aa627f44cd0393d7df3606d8ee8bc9adf4
chiefexb <chief_exb@mail.ru>
2014-07-19 18:49:30 +0400

    update 19.07.2014 Вставляет одну запись из файла заданного кодом. Надо сделать чтобы файлы были из папки и шел перебор, перенос файлов в архивную папку

--- upload.py
+++ upload.py
@@ -35,6 +35,7 @@ def main():
  rbd_user=rbd_database.find('user').text
  rbd_password=rbd_database.find('password').text
  rbd_host=rbd_database.find('hostname').text
+ clm=' , '
  
  try:
    con = fdb.connect (host=main_host, database=main_dbname, user=main_user, password=main_password,charset='WIN1251')
@@ -45,7 +46,8 @@ def main():
 
  print input_path
  #input_path='/home/chief/work/gai/'
- f=file(input_path+'for_UFSSP_20140609_093724.txt')
+ ff='for_UFSSP_20131219_184058.txt'
+ f=file(input_path+ff)
  l=f.readlines()
  periodstr=l[3].decode('CP1251').partition(u'период (дата технологической операции) :')
  pp=periodstr[2].lstrip(' ')
@@ -57,12 +59,17 @@ def main():
  date_act=l[5].decode('CP1251').partition(u'Дата и время формирования :')[2].lstrip(' ').split(' ')[0]
  print  date_act
  pp=l[6].decode('CP1251').split(';')
- print pp[1]
- sql='INSERT INTO REESTRS (ID, NUM_ID, DATE_ID, LASTNAME, FISTNAME_OGRN, SECONDNAME_INN, SUMM, DATE_ISP, MARK_ISP, PRIM, FILENAME, DATE_ACT, PERIOD_START, PERIOD_END, FIO_OFICER, STATUS, OSP, NUM_IP) VALUES ('
+ print pp[1],pp
+ sql='INSERT INTO REESTRS (ID, PACKET_ID, NUM_ID, DATE_ID, LASTNAME, FISTNAME_OGRN, SECONDNAME_INN, SUMM, DATE_ISP, MARK_ISP, PRIM, FILENAME, DATE_ACT, PERIOD_START, PERIOD_END, FIO_OFICER, STATUS, OSP, NUM_IP) VALUES ('
  cur = con.cursor()
  id=getgenerator(cur,'SEC_REESTRS')
+ pack_id=getgenerator(cur,'SEC_REESTRS_PACK')
  print id
- sql=sql+str(id)+','
+ 
+ sql=sql+str(id)+clm+str(pack_id)+clm+quoted(pp[1])+clm+quoted(pp[2])+clm+quoted(pp[3])+clm+quoted(pp[4])+clm+quoted(pp[5])+clm+(pp[6])+clm+quoted(pp[7])+clm+quoted(pp[8])+clm+quoted(pp[9])+clm+quoted(ff)+clm+quoted(date_act)+clm+quoted(date_start)+clm+quoted(date_end)+clm+quoted(oficer)+',null, null,null)'
+ print sql
+ cur.execute(sql)
+ con.commit()
  f.close()
  fileconfig.close()
  con.close() 
